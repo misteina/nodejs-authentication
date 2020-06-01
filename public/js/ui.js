e9597dc2332e1ad03d66dc61b04b2cb87524a078
Enyinna <enyinna.tech@gmail.com>
2020-06-01 21:18:55 +0100

    Login page design

---
+++ public/js/ui.js
@@ -0,0 +1,9 @@
+/*'use strict';
+
+//npx babel--watch react--out - dir public / js--presets react - app / prod
+
+function App(){
+    return <span>Hello there!</span>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));*/
